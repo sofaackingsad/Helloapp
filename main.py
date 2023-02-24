e075675d4dc3cbe22131cba38443af7fd73b4d4b
sofaackingsad <peterson.sonia@mail.ru>
2023-02-25 01:11:20 +0300

    Add exersice

--- main.py
+++ main.py
@@ -2,3 +2,21 @@ print("Hello Python from PyCharm")
 s = "*" *30
 print("New project")
 print(s)
+
+#Написать алгоритм генерации пароля.
+
+import random
+chars='abcdefghijklmnopkrstunvwxyzABCDEFGHIJKLMNOPQRSTUNVWXYZ0123456789!.,/?\|=+-_)(*&^%$#@~'
+try:
+    password=''
+    len=int(input("Длина пароля от 8 до 20: "))
+    if len < 8:
+        print('Неверный ввод!')
+    elif len > 20:
+        print('Неверный ввод!')
+    elif len >=8 and len <=20:
+        for i in range(len):
+            password+=random.choice(chars);
+        print('Пароль получился:\n',password)
+except ValueError:
+    print('Длина пароля должна состоять из целочисленных цифр!')
